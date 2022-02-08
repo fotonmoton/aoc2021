81f11c52ef9ee2ecfe07a9b39c1d8767fade23a7
Gregory <neogen570@gmail.com>
2022-02-08 21:38:01 +0200

    d3

---
+++ d3/d3_test.go
@@ -0,0 +1,25 @@
+package d3
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestD3(t *testing.T) {
+	input := `
+00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010`
+
+	assert.EqualValues(t, 198, D3(input))
+}
