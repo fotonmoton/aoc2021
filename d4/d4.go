72dc38807d0ac595418bd67e5fa78e6a3f7efc1d
Gregory <neogen570@gmail.com>
2022-02-13 20:14:28 +0200

    d4 and d4p4

---
+++ d4/d4.go
@@ -0,0 +1,132 @@
+package d4
+
+import (
+	"strconv"
+	"strings"
+)
+
+type cell struct {
+	value  int
+	marked bool
+}
+
+type board []cell
+
+const SIDE_SIZE = 5
+
+func createBoards(rawBoards []string) []board {
+	boards := make([]board, len(rawBoards))
+
+	for i, rawBoard := range rawBoards {
+		for _, cellRow := range strings.Split(rawBoard, "\n") {
+			for _, rawCell := range strings.Split(cellRow, " ") {
+				if rawCell != "" {
+					value, _ := strconv.Atoi(rawCell)
+					boards[i] = append(boards[i], cell{value, false})
+				}
+			}
+		}
+	}
+
+	return boards
+}
+
+func markBoard(b board, take int) board {
+	for i := range b {
+		if b[i].value == take {
+			b[i].marked = true
+		}
+	}
+
+	return b
+}
+
+func gotBingo(b board) bool {
+
+	for i := 0; i < SIDE_SIZE; i++ {
+		if b[i*SIDE_SIZE].marked &&
+			b[i*SIDE_SIZE+1].marked &&
+			b[i*SIDE_SIZE+2].marked &&
+			b[i*SIDE_SIZE+3].marked &&
+			b[i*SIDE_SIZE+4].marked {
+			return true
+		}
+	}
+
+	for j := 0; j < SIDE_SIZE; j++ {
+		if b[j].marked &&
+			b[j+SIDE_SIZE].marked &&
+			b[j+SIDE_SIZE*2].marked &&
+			b[j+SIDE_SIZE*3].marked &&
+			b[j+SIDE_SIZE*4].marked {
+			return true
+		}
+	}
+
+	return false
+}
+
+func findBingo(boards []board, takes []string) (board, int) {
+
+	for _, take := range takes {
+		converted, _ := strconv.Atoi(take)
+		for _, board := range boards {
+			marked := markBoard(board, converted)
+			if gotBingo(marked) {
+				return marked, converted
+			}
+		}
+	}
+
+	return nil, 0
+}
+
+func findBingo2(boards []board, takes []string) (board, int) {
+	bingos := make(map[*board]int)
+	wins := []*board{}
+	for _, take := range takes {
+		converted, _ := strconv.Atoi(take)
+		for boardIndex := range boards {
+			board := &boards[boardIndex]
+			if _, ok := bingos[board]; !ok {
+				marked := markBoard(*board, converted)
+				if gotBingo(marked) {
+					bingos[board] = converted
+					wins = append(wins, board)
+				}
+			}
+		}
+	}
+
+	return *wins[len(wins)-1], bingos[wins[len(wins)-1]]
+}
+
+func score(b board, take int) int {
+	sum := 0
+
+	for _, cell := range b {
+		if !cell.marked {
+			sum += cell.value
+		}
+	}
+
+	return sum * take
+}
+
+func D4(input string) int {
+	lines := strings.Split(input, "\n\n")
+	takes := strings.Split(lines[0], ",")
+	boards := createBoards(lines[1:])
+	bingo, take := findBingo(boards, takes)
+
+	return score(bingo, take)
+}
+
+func D4P2(input string) int {
+	lines := strings.Split(input, "\n\n")
+	takes := strings.Split(lines[0], ",")
+	boards := createBoards(lines[1:])
+	bingo, take := findBingo2(boards, takes)
+
+	return score(bingo, take)
+}
--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/fotonmoton/aoc2021/d1"
 	"github.com/fotonmoton/aoc2021/d2"
 	"github.com/fotonmoton/aoc2021/d3"
+	"github.com/fotonmoton/aoc2021/d4"
 )
 
 func main() {
@@ -28,5 +29,6 @@ func main() {
 	fmt.Printf("day 2 part 2: %v\n", d2.D2P2(client.Day(2)))
 	fmt.Printf("day 3: %v\n", d3.D3(client.Day(3)))
 	fmt.Printf("day 3 part 2: %v\n", d3.D3P2(client.Day(3)))
-
+	fmt.Printf("day 4: %v\n", d4.D4(client.Day(4)))
+	fmt.Printf("day 4 part 2: %v\n", d4.D4P2(client.Day(4)))
 }
